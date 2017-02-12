21afdcf625ba08afcac51e5d10acdb12cf69e6ea
Arkadiy Shinkarev <shinkarev@me.com>
2017-02-12 18:54:30 +0300

    Update home-brew install URL

--- osx_install.sh
+++ osx_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 source .brew
 source .osx
-
